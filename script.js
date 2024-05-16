9386a6138d57768ec6c456a9d9e65914c08709a7
Mariam Lobzhanidze <163752402+MariamLobzhanidze@users.noreply.github.com>
2024-05-16 13:30:00 +0400

    work 1 and 2

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 // 2. ჯავასკრიპტით შევქმნათ img tag რომელსაც src ად მივანიჭებთ ამ: https://www.simplilearn.com/ice9/free_resources_article_thumb/Become_a_Front_End_Developer.jpg სურათის ლინკს და ეს შექმნილი img ჩავსვათ body ში (ისე როგორც ლექციაზე გავაკეთეთ). გამოიყენეთ ეს მეთოდი setAttribute() https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute
 
 
-
+const image = document.createElement("img");
+image.setAttribute(
+  "src",
+  "https://www.simplilearn.com/ice9/free_resources_article_thumb/Become_a_Front_End_Developer.jpg"
+);
+image.setAttribute("studying", "codding");
+image.style.width = "300px";
+document.body.appendChild(image);
 
 // 3. დაამატეთ ერთი ინფუთი და ღილაკი , რა ინფორმაციასაც  შეიყვანთ ინფუთის ველში, ღილაკზე დაჭერისას ქვემოთ p ტეგში  გამოიტანეთ ინფუთის მნიშვნელობა. გამოიყენეთ ეს მეთოდი https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/change_event
 
